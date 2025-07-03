25745b9d098272579cffeb3bda53e7c3498bc17f
lvovl7070 <lvovl7070@naver.com>
2025-07-03 17:37:57 +0900

    Initial Commit

---
+++ CheckEat-User/CheckEat_UserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckEat_UserApp.swift
+//  CheckEat-User
+//
+//  Created by Hee  on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CheckEat_UserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
