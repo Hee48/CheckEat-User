07b005d5685d5b7f3f0f56e4592c9c6f356b2a92
Hee_Jo <152968348+Hee48@users.noreply.github.com>
2025-07-23 16:41:12 +0900

    Update API.swift

--- CheckEat-User/Models/API.swift
+++ CheckEat-User/Models/API.swift
@@ -7,11 +7,5 @@
 import Foundation
 
 enum API {
-    static let endPoint = "http://localhost:3000/"
-    static let loginURL = endPoint + "auth/login"
-    static let checkIDUniqueURL = endPoint + "auth/check-id-unique"
-    static let checkEmailUniqueURL = endPoint + "auth/check-email-unique"
-    static let sendEmailTokenURL = endPoint + "auth/send-email-token"
-    static let checkEmailTokenURL = endPoint + "auth/check-email-token"
-    static let signUpURL = endPoint + "auth/signup/user"
+
 }
